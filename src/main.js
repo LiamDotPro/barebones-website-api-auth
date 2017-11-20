3ea2bef4d4754c0fe3a0b8192a276b09d864d2ab
Liam Read <liam.read@students.plymouth.ac.uk>
2017-11-20 20:39:15 +0000

    Api Cleanup

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import VueSocketIo from 'vue-socket.io'
 import VueLocalStorage from 'vue-ls'
 
 Vue.use(VueLocalStorage)
+Vue.use(VueSocketIo, 'http://localhost:2001')
+// Vue.use(VueSocketIo, socketio('http://localhost:2000', store))
 
 Vue.config.productionTip = false
 
